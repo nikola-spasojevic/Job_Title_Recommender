6d9aa9bd861839749fce2db65b47d6aa90a43d0f
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2019-04-05 14:29:55 +0200

    added condirional computation if pickled states already present in bin folder

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from src.text_preprocessing import TextPreprocessing
-from src.bag_of_words import BagOfWords
-from src.language_model import NgramLanguageModel
-from src.job_title_recommender import JobTitleRecommender
+from src.bag_of_words import BagOfWords, CORPUS_DIR
+from src.language_model import NgramLanguageModel, NGRAMS_DIR
+from src.job_title_recommender import JobTitleRecommender, LIKELIHOODS_DIR, UNIGRAM_FREQ_DIR
+import os
 
 def main():
-	TextPreprocessing.corpus_gen()
-	BagOfWords.ngram_frequencies_gen()
-	NgramLanguageModel.calc_likelihood()
+	if not os.path.isfile(CORPUS_DIR):
+		TextPreprocessing.corpus_gen()
+	if not os.path.isfile(UNIGRAM_FREQ_DIR) or not os.path.isfile(NGRAMS_DIR):
+		BagOfWords.ngram_frequencies_gen()
+	if not os.path.isfile(LIKELIHOODS_DIR):
+		NgramLanguageModel.calc_likelihood()
+	
 	job_title_recommender = JobTitleRecommender()
-	print(job_title_recommender.auto_complete('engin'))
-	print(job_title_recommender.propose_next_word('ajksd ajskdk jkasdn java engineering'))
 
+	print(job_title_recommender.auto_complete('jav'))
+	print(job_title_recommender.propose_next_word('gibberish software'))
+
+
+	# Evaluation to be done using word error rate (WER. using edit distance
 if __name__ == "__main__":
    main()
