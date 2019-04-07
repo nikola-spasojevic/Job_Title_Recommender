71b481b57d97bcb7abdb6704994496cbd8bdd715
Nikola Spasojevic <nikola.t.spasojevic@gmail.com>
2019-04-07 20:00:19 +0200

    updated traing corpus directory

--- src/helper/tokenizer.py
+++ src/helper/tokenizer.py
@@ -1,10 +1,8 @@
 import pickle
 
-TRAIN_CORPUS_DIR = 'bin/train_corpus.pkl'
-
 class Tokenizer:
 	@staticmethod
-	def job_data_tokenizer(train_corpus_dir=TRAIN_CORPUS_DIR):
+	def job_data_tokenizer(train_corpus_dir):
 		with open(train_corpus_dir, 'rb') as pickle_in:
 			train_corpus = pickle.load(pickle_in, encoding='utf8')
 
@@ -13,9 +11,3 @@ class Tokenizer:
 			tokenized_train_corpus.append(i.split())
 
 		return tokenized_train_corpus
-
-def main():
-	Tokenizer.job_data_tokenizer()
-
-if __name__ == "__main__":
-   main()
